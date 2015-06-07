efe7cf950106bf42a63ea2c5b28fb18a9ab43b14
Nan Chen <nchen333@gmail.com>
2015-06-07 00:31:56 -0400

    posting on server

--- main.js
+++ main.js
@@ -5,52 +5,84 @@
 
   $(document).ready(function(){
 
-    route();
+    // route();
 
 
-    $(window).on('hashchange', function(event){
+    $(document).on("submit", ".chat-form", function(event) {
       event.preventDefault();
-      route();
+      var text = $(this).find('textarea').val();
+
     });
-  });
 
-  function route() {
-  switch(window.location.hash) {
-    case '':
-      $('.posts').html(JST['login']());
-      break;
-    case '#/chat':
-      renderChat();
-      break;
-  }
-}
+    function renderMessages(messages){
+      $('.messages-container').html(JST['messages'](messages));
 
-function renderChat() {
-  $('.posts').html(JST['chat']());
-  console.log('username:', username);
-  fetchMessages()
-}
-
-
-
-
-}
-
-function renderMessages(messages){
-  $('.messages-container').html(JST['messages'](messages));
-
-}
-
-$(function(){
-  $("#textbox").keypress(function(event){
-    if(event.which === 13) {
-      event.preventDefault();
     }
+
+    $(function(){
+      $("#textbox").keypress(function(event){
+        if(event.which === 13) {
+          event.preventDefault();
+        }
+      });
+      $("#button1").click(function() {
+        var newMessage = $("#textbox1").val();
+        $("#textbox1").val("");
+        var prevState = $("#first-post").html();
+        $("#first-post").html(prevState + "<br>" + newMessage);
+        $.ajax({
+        url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/",
+        type: "POST",
+        data: {
+          content: newMessage
+        }
+      });
+      });
+    });
+
+
   });
-  $("#button").click(function() {
-    var newMessage = $("#textbox").val();
-    $("#textbox").val("");
-    var prevState = $(".messages-container").html();
-    $(".messages-container").html(prevState + "<br>" + newMessage);
-  });
-});
+//     $(window).on('hashchange', function(event){
+//       event.preventDefault();
+//       route();
+//     });
+//   });
+//
+//   function route() {
+//   switch(window.location.hash) {
+//     case '':
+//       $('.posts').html(JST['login']());
+//       break;
+//     case '#/chat':
+//       renderChat();
+//       break;
+//   }
+// }
+
+// function renderChat() {
+//   $('.posts').html(JST['chat']());
+//   console.log('username:', username);
+//   fetchMessages()
+// }
+//
+// var menuId = $(".posts").first().attr("id");
+// var request = $.ajax({
+//   // url: ,
+//   type: "POST",
+//   data: {id : menuId},
+//   dataType: "json"
+// });
+
+// request.done(function(msg) {
+//   $("#log").html( msg );
+// });
+//
+// request.fail(function(jqXHR, textStatus) {
+//   alert( "Request failed: " + textStatus );
+// });
+
+
+
+
+
+}) ();
